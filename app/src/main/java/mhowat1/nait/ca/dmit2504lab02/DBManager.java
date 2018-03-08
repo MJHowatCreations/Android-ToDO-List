512e45440d0cf2722436c8f6c97876a304e2eab9
Matthew Howat <matthewj.howat@gmail.com>
2018-03-08 14:56:40 -0700

    EndofThursdayMarch8

--- app/src/main/java/mhowat1/nait/ca/dmit2504lab02/DBManager.java
+++ app/src/main/java/mhowat1/nait/ca/dmit2504lab02/DBManager.java
@@ -1,10 +1,14 @@
 package mhowat1.nait.ca.dmit2504lab02;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mhowat1 on 2/28/2018.
@@ -13,25 +17,44 @@ import android.util.Log;
 public class DBManager extends SQLiteOpenHelper {
 
     static final String TAG = "DBManager";
-    static final String DB_NAME = "chatter.db";
+    static final String DB_NAME = "todolist.db";
     static private final int DB_VERSION = 1;
-    static final String LISTITEM_TABLE = "listitem";
-    static final String C_ID = BaseColumns._ID;
-    static final String C_DATE = "postDate";
-    static final String C_SENDER = "sender";
-    static final String C_DATA = "data";
+
+    static final String LIST_TABLE = "list";
+    static final String C_LISTID = BaseColumns._ID;
+    static final String C_LISTNAME = "name";
+
+    static final String ITEM_TABLE = "item";
+    static final String C_ITEMID = BaseColumns._ID;
+    static final String C_ITEMLISTFK = "listID";
+    static final String C_ITEMNAME = "name";
+    static final String C_ITEMDESCRIPTION = "description";
+    static final String C_ITEMDATE = "date";
+    static final String C_ITEMDONE = "done";
+    static final String C_ITEMARCHIEVE = "archieve";
 
 
     public DBManager(Context context){super(context, DB_NAME, null, DB_VERSION);}
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        String sql = "create table " + TABLE + " (" + C_ID + " int primary key, "
-                + C_DATE + " text, " + C_SENDER + " text, " + C_DATA + " text)";
+        String listSql = "create table " + LIST_TABLE + " (" + C_LISTID + " int primary key autoincrement, "
+                + C_LISTNAME + " text)";
 
-        Log.d(TAG,sql);
+        String itemSql = "create table " + ITEM_TABLE +
+                " (" + C_ITEMID + " int primary key autoincrement, " +
+                       C_ITEMLISTFK + " int, " +
+                       C_ITEMNAME + " text, " +
+                       C_ITEMDESCRIPTION + " text, " +
+                       C_ITEMDATE + " text, " +
+                       C_ITEMDONE + " text, " +
+                       C_ITEMARCHIEVE + " text)";
 
-        database.execSQL(sql);
+        Log.d(TAG,listSql);
+        Log.d(TAG,itemSql);
+
+        database.execSQL(listSql);
+        database.execSQL(itemSql);
 
 
     }
@@ -39,9 +62,26 @@ public class DBManager extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
 
-        database.execSQL("drop table if exists " + TABLE); // drops the old database
+        database.execSQL("drop table if exists " + LIST_TABLE); // drops the old database
+        database.execSQL("drop table if exists " + ITEM_TABLE);
         Log.d(TAG, "onUpdated");
         onCreate(database);
 
+    }
+
+    public List<String> getAllLists(){
+        List<String> lists = new ArrayList<String>();
+        String selectQuery = "SELECT * FROM " + LIST_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()){
+            do{
+                lists.add(cursor.getString(1));
+            }while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+
+        return lists;
     }
 }
--- app/src/main/java/mhowat1/nait/ca/dmit2504lab02/MainActivity.java
+++ app/src/main/java/mhowat1/nait/ca/dmit2504lab02/MainActivity.java
@@ -2,12 +2,57 @@ package mhowat1.nait.ca.dmit2504lab02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+    Spinner listSpinner;
+    Button btnAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listSpinner = (Spinner) findViewById(R.id.main_activity_spinner);
+        btnAdd = (Button) findViewById(R.id.main_activity_add_button);
+
+
+        btnAdd.setOnClickListener(this);
+        listSpinner.setOnItemSelectedListener(this);
+        loadSpinnerData();
+
+    }
+
+    private void loadSpinnerData() {
+        DBManager db = new DBManager(this);
+        List<String> lists = db.getAllLists();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lists);
+        dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        listSpinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onClick(View v){
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
