31374b7bc516d0293c3dc94444bf8ec1c11cf749
Matthew <matthewj.howat@gmail.com>
2018-03-12 09:51:22 -0600

    MondayClass

--- app/src/main/java/mhowat1/nait/ca/dmit2504lab02/DBManager.java
+++ app/src/main/java/mhowat1/nait/ca/dmit2504lab02/DBManager.java
@@ -9,6 +9,7 @@ import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.DelayQueue;
 
 /**
  * Created by mhowat1 on 2/28/2018.
@@ -19,6 +20,7 @@ public class DBManager extends SQLiteOpenHelper {
     static final String TAG = "DBManager";
     static final String DB_NAME = "todolist.db";
     static private final int DB_VERSION = 1;
+
 
     static final String LIST_TABLE = "list";
     static final String C_LISTID = BaseColumns._ID;
@@ -38,17 +40,18 @@ public class DBManager extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        String listSql = "create table " + LIST_TABLE + " (" + C_LISTID + " int primary key autoincrement, "
+        String listSql = "create table " + LIST_TABLE + " (" + C_LISTID + " integer primary key autoincrement, "
                 + C_LISTNAME + " text)";
 
         String itemSql = "create table " + ITEM_TABLE +
-                " (" + C_ITEMID + " int primary key autoincrement, " +
+                " (" + C_ITEMID + " integer primary key autoincrement, " +
                        C_ITEMLISTFK + " int, " +
                        C_ITEMNAME + " text, " +
                        C_ITEMDESCRIPTION + " text, " +
                        C_ITEMDATE + " text, " +
                        C_ITEMDONE + " text, " +
                        C_ITEMARCHIEVE + " text)";
+
 
         Log.d(TAG,listSql);
         Log.d(TAG,itemSql);
@@ -71,17 +74,18 @@ public class DBManager extends SQLiteOpenHelper {
 
     public List<String> getAllLists(){
         List<String> lists = new ArrayList<String>();
-        String selectQuery = "SELECT * FROM " + LIST_TABLE;
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
-        if (cursor.moveToFirst()){
-            do{
-                lists.add(cursor.getString(1));
+
+        Cursor cursor = db.query(LIST_TABLE, null, null, null, null, null, C_LISTID + " DESC");
+        if(cursor.moveToFirst())
+        {
+            do {
+                lists.add(cursor.getString(0));
             }while (cursor.moveToNext());
         }
+
         cursor.close();
         db.close();
-
         return lists;
     }
 }
--- app/src/main/java/mhowat1/nait/ca/dmit2504lab02/MainActivity.java
+++ app/src/main/java/mhowat1/nait/ca/dmit2504lab02/MainActivity.java
@@ -1,20 +1,30 @@
 package mhowat1.nait.ca.dmit2504lab02;
 
+import android.content.ContentValues;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener{
+    private static final String TAG = "MainActivity";
+    SQLiteDatabase db;
+    List<String> lists;
     Spinner listSpinner;
-    Button btnAdd;
+    DBManager dbManager;
+
 
 
     @Override
@@ -23,27 +33,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         listSpinner = (Spinner) findViewById(R.id.main_activity_spinner);
-        btnAdd = (Button) findViewById(R.id.main_activity_add_button);
 
+        Button btnAdd = (Button) findViewById(R.id.main_activity_add_button);
+        Button btnEdit = (Button) findViewById(R.id.main_activity_edit_button);
+
+        dbManager = new DBManager(this);
 
         btnAdd.setOnClickListener(this);
+        btnEdit.setOnClickListener(this);
+
         listSpinner.setOnItemSelectedListener(this);
         loadSpinnerData();
 
     }
 
-    private void loadSpinnerData() {
-        DBManager db = new DBManager(this);
-        List<String> lists = db.getAllLists();
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lists);
-        dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
 
-        listSpinner.setAdapter(dataAdapter);
+
+    private void loadSpinnerData() {
+        try {
+            lists = dbManager.getAllLists();
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, lists.);
+            listSpinner.setAdapter(dataAdapter);
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(this, "Error:" + e, Toast.LENGTH_LONG).show();
+        }
     }
 
-    @Override
-    public void onClick(View v){
 
+
+
+    @Override
+    public void onClick(View view){
+
+        switch(view.getId())
+        {
+            case R.id.main_activity_add_button:
+            {
+                addNewListItem();
+                break;
+            }
+            case R.id.main_activity_edit_button:
+            {
+
+                break;
+            }
+        }
+
+    }
+
+
+    private void addNewListItem() {
+        EditText newListEditText = (EditText)findViewById(R.id.main_activity_edit_text);
+        String newListName = newListEditText.getText().toString();
+        if (newListName.trim() == "" )
+        {
+            Log.d(TAG, "new list name is empty");
+            Toast.makeText(this, "Error: Must have a longer name for list", Toast.LENGTH_LONG).show();
+        }
+        else {
+            try {
+                ContentValues values = new ContentValues();
+                values.put(DBManager.C_LISTNAME, newListName);
+                db = dbManager.getWritableDatabase();
+                db.insertOrThrow(DBManager.LIST_TABLE, null, values);
+                Log.d(TAG, "record inserted");
+                db.close();
+
+            } catch (SQLException e) {
+                Log.d(TAG, "duplicate record");
+
+            }
+        }
+        loadSpinnerData();
     }
 
     @Override
@@ -55,4 +118,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+
 }
